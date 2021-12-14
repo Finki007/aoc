ffdb8b8be700a0948a476b1e0d9f56a3389403b1
Marcel Finkbeiner <marcel.finkbeiner@zoi.de>
2021-12-14 21:26:45 +0100

    add day 14

---
+++ 2021/14/example.js
@@ -0,0 +1,56 @@
+var [polymer_template, pair_insertion] = `NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`.toString().split('\n\n');
+
+pair_insertion = pair_insertion.split('\n').reduce((p,c) => {
+  let [key, value] = c.split(' -> ');
+  p[key] = value;
+  return p;
+}, {});
+
+var polymer = polymer_template.split('').reduce((p,c,i) => {
+  if (i === 0)
+    return p;
+  p[polymer_template[i-1] + c] = 1;
+  return p;
+}, {})
+
+const calc_step = (steps) => {
+  let unique = polymer_template.split('').reduce((p,c) => {
+    p[c] = !p[c] ? 1 : p[c] + 1;
+    return p;
+  }, {});
+  let p = polymer;
+  for (let _ = 0; _ < steps; _++) {
+    let _polymer = {};
+    for (let [_p,_v] of Object.entries(p)) {
+      let _i = pair_insertion[_p];
+      _polymer[_p[0] + _i] = _polymer[_p[0] + _i] ? _polymer[_p[0] + _i] + _v : _v;
+      _polymer[_i + _p[1]] = _polymer[_i + _p[1]] ? _polymer[_i + _p[1]] + _v : _v;
+      unique[_i] = !unique[_i] ? _v : unique[_i] + _v;
+    }
+    p = _polymer;
+  }
+  let elements = Object.entries(unique).sort((a,b) => b[1] - a[1]);
+  return elements[0][1] - elements.slice(-1).pop()[1]
+}
+
+const part1 = calc_step(10);
+console.log("solution: " + part1);  //solution: 1588
+const part2 = calc_step(40);
+console.log("solution: " + part2);  //solution: 2188189693529
